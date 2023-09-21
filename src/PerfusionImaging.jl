3d487a51cc8c7c41570481b38078506e025ee57d
Dale Black <djblack@uci.edu>
2023-09-21 12:36:10 -0700

    initialize repo

--- src/PerfusionImaging.jl
+++ src/PerfusionImaging.jl
@@ -1,5 +1,21 @@
 module PerfusionImaging
 
-# Write your package code here.
+using PythonCall
+
+const sitk = PythonCall.pynew()
+const np = PythonCall.pynew()
+
+function __init__()
+    PythonCall.pycopy!(sitk, pyimport("SimpleITK"))
+    PythonCall.pycopy!(np, pyimport("numpy"))
+end
+
+export sitk, np
+
+include("arterial_input_function.jl")
+include("gamma_variate.jl")
+include("masking.jl")
+include("registration.jl")
+include("utils.jl")
 
 end
