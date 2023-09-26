b6b1747853bbcc24c866deb0ee20ee97bcc1d0f2
Dale Black <djblack@uci.edu>
2023-09-25 17:47:51 -0700

    update example to github url

--- examples/limb_perfusion.jl
+++ examples/limb_perfusion.jl
@@ -19,14 +19,13 @@ end
 begin
     using Pkg; Pkg.activate(; temp = true)
 
-	Pkg.add("CondaPkg")
     Pkg.add("DICOM")
     Pkg.add("CairoMakie")
     Pkg.add("PlutoUI")
     Pkg.add("ImageMorphology")
     Pkg.add("Statistics")
     Pkg.add("DataFrames")
-	Pkg.develop(path = "/Users/daleblack/Library/CloudStorage/GoogleDrive-djblack@uci.edu/My Drive/dev/julia/PerfusionImaging")
+	Pkg.add(url = "https://github.com/Dale-Black/PerfusionImaging.jl")
 
     using DICOM, CairoMakie, PlutoUI, ImageMorphology, Statistics, DataFrames
 	using PerfusionImaging
@@ -270,7 +269,7 @@ md"""
 ss_arr = load_dcm_array(dcms_ss);
 
 # ╔═╡ 25a72dd0-7435-4aa9-98b0-d8a6f5ea1eed
-@bind z_ss PlutoUI.Slider(axes(ss_arr, 3), show_value = true, default = size(ss_arr, 3) ÷ 2)
+@bind z_ss PlutoUI.Slider(axes(ss_arr, 3), show_value = true, default = size(ss_arr, 3))
 
 # ╔═╡ 36be9348-d572-4fa5-86e5-6e529f2d7092
 let
@@ -410,6 +409,8 @@ Choose x location: $(@bind x1_aif PlutoUI.Slider(axes(ss_arr, 1), show_value = t
 Choose y location: $(@bind y1_aif PlutoUI.Slider(axes(ss_arr, 1), show_value = true, default = size(ss_arr, 1) ÷ 2))
 
 Choose radius: $(@bind r1_aif PlutoUI.Slider(1:10, show_value = true))
+
+Check box when ready: $(@bind aif1_ready PlutoUI.CheckBox())
 """
 
 # ╔═╡ 5eb279b5-348f-4c00-bad2-c40f545739be
@@ -446,6 +447,8 @@ Choose x location: $(@bind x2_aif PlutoUI.Slider(axes(v2_reg, 1), show_value = t
 Choose y location: $(@bind y2_aif PlutoUI.Slider(axes(v2_reg, 1), show_value = true, default = size(v2_reg, 1) ÷ 2))
 
 Choose radius: $(@bind r2_aif PlutoUI.Slider(1:10, show_value = true))
+
+Check box when ready: $(@bind aif2_ready PlutoUI.CheckBox())
 """
 
 # ╔═╡ e9dad16b-07bc-4b87-be6b-959b078a5ba7
@@ -468,14 +471,12 @@ let
 	f
 end
 
-# ╔═╡ 1f411b88-7447-4281-a17f-22586aa9f503
-aif_vec_ss = compute_aif(ss_arr, x1_aif, y1_aif, r1_aif)
-
-# ╔═╡ e3999ac3-5bb1-443f-916b-1f40ff080db8
-aif_vec_v2 = compute_aif(v2_reg, x2_aif, y2_aif, r2_aif, z2_aif)
-
-# ╔═╡ 7cfa63ad-2365-4256-a75d-251e1033883d
-aif_vec_gamma = [aif_vec_ss..., aif_vec_v2]
+# ╔═╡ 6864be65-b474-4fa2-84a6-0f8c789e669d
+if aif1_ready && aif2_ready
+	aif_vec_ss = compute_aif(ss_arr, x1_aif, y1_aif, r1_aif)
+	aif_vec_v2 = compute_aif(v2_reg, x2_aif, y2_aif, r2_aif, z2_aif)
+	aif_vec_gamma = [aif_vec_ss..., aif_vec_v2]
+end
 
 # ╔═╡ 1e21f95a-bc8a-492f-9a56-820dd3b3d066
 md"""
@@ -487,23 +488,18 @@ md"""
 ## Extract SureStart Times
 """
 
-# ╔═╡ 731d29ce-4dee-418a-95f7-42aacc073ae7
-begin
+# ╔═╡ 61604f83-e5f3-4aed-ac0b-1c630d7a1d67
+if aif1_ready && aif2_ready
 	time_vector_ss = scan_time_vector(dcms_ss)
 	time_vector_ss_rel = time_vector_ss .- time_vector_ss[1]
-end
 
-# ╔═╡ 71f85cd3-020b-46bb-8dd4-483f6125bf38
-begin
 	time_vector_v2 = scan_time_vector(dcms_v2)
 	time_vector_v2_rel = time_vector_v2 .- time_vector_v2[1]
+
+	delta_time = time_vector_v2[length(time_vector_v2) ÷ 2] - time_vector_ss[end]
+
+	time_vec_gamma = [time_vector_ss_rel..., delta_time + time_vector_ss_rel[end]]
 end
-
-# ╔═╡ 692d455a-a746-4913-a7ce-5fd30d6718ae
-delta_time = time_vector_v2[length(time_vector_v2) ÷ 2] - time_vector_ss[end]
-
-# ╔═╡ 2975eb4b-3dbf-4201-8d2a-94d95c417570
-time_vec_gamma = [time_vector_ss_rel..., delta_time + time_vector_ss_rel[end]]
 
 # ╔═╡ e385d115-47e4-4a59-a6d0-6ea95455e901
 md"""
@@ -511,7 +507,7 @@ md"""
 """
 
 # ╔═╡ e87721fa-5731-4a3e-bd8d-a17dc8fdeffc
-begin
+if aif1_ready && aif2_ready
 	# Upper and Lower Bounds
 	lb = [-100.0, 0.0]
 	ub = [100.0, 200.0]
@@ -520,67 +516,59 @@ begin
 	p0 = [0.0, baseline_hu]  # Initial guess (0, blood pool offset)
 
 	time_vec_end, aif_vec_end = time_vec_gamma[end], aif_vec_gamma[end]
+
+	fit = gamma_curve_fit(time_vec_gamma, aif_vec_gamma, time_vec_end, aif_vec_end, p0; lower_bounds = lb, upper_bounds = ub)
+	opt_params = fit.param
 end
 
-# ╔═╡ 93002fa1-995c-4306-a6a6-264be2d939d1
-fit = gamma_curve_fit(time_vec_gamma, aif_vec_gamma, time_vec_end, aif_vec_end, p0; lower_bounds = lb, upper_bounds = ub)
-
-# ╔═╡ 54916f43-c05a-4ff3-bd62-ba715f0b6937
-opt_params = coef(fit)
-
 # ╔═╡ fc43feee-9d9a-4af6-a76a-7dfbb927c0ae
-begin
+if aif1_ready && aif2_ready
 	x_fit = range(start = minimum(time_vec_gamma), stop = maximum(time_vec_gamma), length=500)
 	y_fit = gamma(x_fit, opt_params, time_vec_end, aif_vec_end)
 	dense_y_fit_adjusted = max.(y_fit .- baseline_hu, 0)
-end
 
-# ╔═╡ 367a0c68-081f-4a57-9a26-9d4094b8f1df
-area_under_curve = trapz(x_fit, dense_y_fit_adjusted)
-
-# ╔═╡ e4ed2262-1d71-47ec-8256-6d5c2f8ad376
-times = collect(range(time_vec_gamma[1], stop=time_vec_end, length=round(Int, maximum(time_vec_gamma))))
-
-# ╔═╡ baaa5740-d6e6-405a-8e14-fa5b0ef7050b
-begin
+	area_under_curve = trapz(x_fit, dense_y_fit_adjusted)
+	times = collect(range(time_vec_gamma[1], stop=time_vec_end, length=round(Int, maximum(time_vec_gamma))))
+	
 	input_conc = area_under_curve ./ (time_vec_gamma[19] - times[4])
 	if length(aif_vec_gamma) > 2
 		input_conc = mean([aif_vec_gamma[end], aif_vec_gamma[end-1]])
 	end
-	input_conc
 end
 
 # ╔═╡ c44b2487-bcd2-43f2-af89-2e3b0e1a54e8
-let
-	f = Figure()
-	ax = Axis(
-		f[1, 1],
-		xlabel = "Time Point",
-		ylabel = "Intensity (HU)",
-		title = "Fitted AIF Curve"
-	)
+if aif1_ready && aif2_ready
+	let
+		f = Figure()
+		ax = Axis(
+			f[1, 1],
+			xlabel = "Time Point",
+			ylabel = "Intensity (HU)",
+			title = "Fitted AIF Curve"
+		)
+		
+		scatter!(time_vec_gamma, aif_vec_gamma, label="Data Points")
+		lines!(x_fit, y_fit, label="Fitted Curve", color = :red)
+		scatter!(time_vec_gamma[end-1], aif_vec_gamma[end-1], label = "Trigger")
+		scatter!(time_vec_gamma[end], aif_vec_gamma[end], label = "V2")
+		
+		axislegend(ax, position=:lt)
 	
-	scatter!(time_vec_gamma, aif_vec_gamma, label="Data Points")
-	lines!(x_fit, y_fit, label="Fitted Curve", color = :red)
-	scatter!(time_vec_gamma[end-1], aif_vec_gamma[end-1], label = "Trigger")
-	scatter!(time_vec_gamma[end], aif_vec_gamma[end], label = "V2")
+		# Create the AUC plot
+		time_temp = range(time_vec_gamma[3], stop=time_vec_gamma[end], length=round(Int, maximum(time_vec_gamma) * 1))
+		auc_area = gamma(time_temp, opt_params, time_vec_end, aif_vec_end) .- baseline_hu
+		
+		# Create a denser AUC plot
+		n_points = 1000  # Number of points for denser interpolation
+		time_temp_dense = range(time_temp[1], stop=time_temp[end], length=n_points)
+		auc_area_dense = gamma(time_temp_dense, opt_params, time_vec_end, aif_vec_end) .- baseline_hu
 	
-	axislegend(ax, position=:lt)
-
-	# Create the AUC plot
-	time_temp = range(time_vec_gamma[3], stop=time_vec_gamma[end], length=round(Int, maximum(time_vec_gamma) * 1))
-	auc_area = gamma(time_temp, opt_params, time_vec_end, aif_vec_end) .- baseline_hu
+		for i = 1:length(auc_area_dense)
+		    lines!(ax, [time_temp_dense[i], time_temp_dense[i]], [baseline_hu, auc_area_dense[i] + baseline_hu], color=:cyan, linewidth=1, alpha=0.2)
+		end
 	
-	# Create a denser AUC plot
-	n_points = 1000  # Number of points for denser interpolation
-	time_temp_dense = range(time_temp[1], stop=time_temp[end], length=n_points)
-	auc_area_dense = gamma(time_temp_dense, opt_params, time_vec_end, aif_vec_end) .- baseline_hu
-
-	for i = 1:length(auc_area_dense)
-	    lines!(ax, [time_temp_dense[i], time_temp_dense[i]], [baseline_hu, auc_area_dense[i] + baseline_hu], color=:cyan, linewidth=1, alpha=0.2)
+		f
 	end
-
-	f
 end
 
 # ╔═╡ 5aecb6b9-a813-4cf8-8a7f-2da4a19a052e
@@ -594,54 +582,48 @@ md"""
 """
 
 # ╔═╡ 140c1343-2a6e-4d4f-a3db-0d608d7e885c
-begin
+if aif1_ready && aif2_ready
 	header = dcms_v1[1].meta
 	voxel_size = get_voxel_size(header)
+	heart_rate = round(1 / (mean(diff(time_vec_gamma)) / 60))
+	tissue_rho = 1.053 # tissue density : g/cm^2
+	organ_mass = (sum(limb_crop) * tissue_rho * voxel_size[1] * voxel_size[2] * voxel_size[3]) / 1000 # g
+	delta_hu = mean(v2_reg[limb_crop]) - mean(v1_crop[limb_crop])
+	organ_vol_inplane =  voxel_size[1] * voxel_size[2] / 1000
+	v1_mass = sum(v1_crop[limb_crop]) * organ_vol_inplane
+	v2_mass = sum(v2_reg[limb_crop]) * organ_vol_inplane
+	flow  = (1 / input_conc) * ((v2_mass - v1_mass) / (delta_time / 60)) # mL/min
+	flow_map = (v2_reg - v1_crop) ./ (mean(v2_reg[limb_crop]) - mean(v1_crop[limb_crop])) .* flow; # mL/min/g, voxel-by-voxel blood perfusion of organ of interest
+	perf_map = flow_map ./ organ_mass;
+	perf = (flow / organ_mass, std(perf_map[limb_crop]))
 end
 
-# ╔═╡ 9a5dfd0a-a27a-444e-9963-621a89170652
-tissue_rho = 1.053 # tissue density : g/cm^2
-
-# ╔═╡ 3631e518-6258-4cb7-ad61-8d7975f2da67
-organ_mass = (sum(limb_crop) * tissue_rho * voxel_size[1] * voxel_size[2] * voxel_size[3]) / 1000 # g
-
-# ╔═╡ 89f21b6f-ecbc-475d-bf9e-408fcbc7f45a
-delta_hu = mean(v2_reg[limb_crop]) - mean(v1_crop[limb_crop])
-
-# ╔═╡ d1fb1e4d-e0b5-48ce-82bd-69b3d91f2b2a
-organ_vol_inplane =  voxel_size[1] * voxel_size[2] / 1000
-
-# ╔═╡ 7b841262-0031-4a44-b17c-f5059b0f59a0
-v1_mass = sum(v1_crop[limb_crop]) * organ_vol_inplane
-
-# ╔═╡ 1b1d2d32-3411-4ecb-8558-a3bf5af10bd8
-v2_mass = sum(v2_reg[limb_crop]) * organ_vol_inplane
-
-# ╔═╡ 4cf12eb6-2739-497c-9171-5f2f557c60d0
-flow  = (1 / input_conc) * ((v2_mass - v1_mass) / (delta_time / 60)) # mL/min
-
-# ╔═╡ 72ddceea-6a7d-4b05-99dd-3313ce0eb840
-flow_map = (v2_reg - v1_crop) ./ (mean(v2_reg[limb_crop]) - mean(v1_crop[limb_crop])) .* flow; # mL/min/g, voxel-by-voxel blood perfusion of organ of interest
-
 # ╔═╡ a2aeaa04-3097-4dd0-8bab-5c98b74514b3
-@bind z6 PlutoUI.Slider(axes(flow_map, 3), show_value = true)
+if aif1_ready && aif2_ready
+	@bind z_flow PlutoUI.Slider(axes(flow_map, 3), show_value = true, default = size(flow_map, 3) ÷ 2)
+end
 
 # ╔═╡ 283c0ee5-0321-4f5d-9792-d593f49cafc1
-heatmap(flow_map[:, :, z6])
+if aif1_ready && aif2_ready
+	let
+		f = Figure()
+		ax = CairoMakie.Axis(
+			f[1, 1],
+			title = "Flow Map"
+		)
+		heatmap!(v2_reg[:, :, z_flow], colormap = :grays)
+		heatmap!(flow_map[:, :, z_flow], colormap = (:jet, 0.6))
 
-# ╔═╡ ed06f353-fdcf-47af-94be-52c2e11957f4
-perf_map = flow_map ./ organ_mass;
-
-# ╔═╡ 15b31020-253d-46e6-a985-f2043c71e1fc
-perf = (flow / organ_mass, std(perf_map[limb_crop]))
+		Colorbar(f[1, 2], limits = (-10, 300), colormap = :jet,
+    flipaxis = false)
+		f
+	end
+end
 
 # ╔═╡ 1befeeba-40bb-4310-8441-6609fc82dc21
 md"""
 ## Results
 """
-
-# ╔═╡ ac0e22da-0617-434c-b6ed-88c5f688f1c1
-heart_rate = round(1 / (mean(diff(time_vec_gamma)) / 60))
 
 # ╔═╡ c0dcbc56-be6e-47ba-b3e8-9f12ec469e4b
 col_names = [
@@ -657,20 +639,24 @@ col_names = [
 ]
 
 # ╔═╡ e79e2a10-78d0-4571-b601-daf9d164b0c9
-col_vals = [
-    perf[1],
-    perf[2],
-    length(perf) == 3 ? perf[3] : missing,
-    perf[1] * organ_mass,
-    perf[2] * organ_mass,  
-    delta_time,
-    organ_mass,
-    delta_hu,
-    heart_rate
-]
+if aif1_ready && aif2_ready
+	col_vals = [
+	    perf[1],
+	    perf[2],
+	    length(perf) == 3 ? perf[3] : missing,
+	    perf[1] * organ_mass,
+	    perf[2] * organ_mass,  
+	    delta_time,
+	    organ_mass,
+	    delta_hu,
+	    heart_rate
+	]
+end
 
 # ╔═╡ a861ded4-dbcf-4f06-a1b4-17d8f8ddf214
-df = DataFrame(parameters = col_names, values = col_vals)
+if aif1_ready && aif2_ready
+	df = DataFrame(parameters = col_names, values = col_vals)
+end
 
 # ╔═╡ Cell order:
 # ╠═eeba971b-c64e-4195-95ca-5cf2ae5ac590
@@ -736,41 +722,20 @@ df = DataFrame(parameters = col_names, values = col_vals)
 # ╟─7f1b44c5-924b-4541-8fff-e1298f9a9ef0
 # ╟─69bbc077-2523-47a1-8c48-b0dc5c8ef0c2
 # ╟─e9dad16b-07bc-4b87-be6b-959b078a5ba7
-# ╠═1f411b88-7447-4281-a17f-22586aa9f503
-# ╠═e3999ac3-5bb1-443f-916b-1f40ff080db8
-# ╠═7cfa63ad-2365-4256-a75d-251e1033883d
+# ╠═6864be65-b474-4fa2-84a6-0f8c789e669d
 # ╟─1e21f95a-bc8a-492f-9a56-820dd3b3d066
 # ╟─0ba3a947-23be-49ca-ac2c-b0d295d096e9
-# ╠═731d29ce-4dee-418a-95f7-42aacc073ae7
-# ╠═71f85cd3-020b-46bb-8dd4-483f6125bf38
-# ╠═692d455a-a746-4913-a7ce-5fd30d6718ae
-# ╠═2975eb4b-3dbf-4201-8d2a-94d95c417570
+# ╠═61604f83-e5f3-4aed-ac0b-1c630d7a1d67
 # ╟─e385d115-47e4-4a59-a6d0-6ea95455e901
 # ╠═e87721fa-5731-4a3e-bd8d-a17dc8fdeffc
-# ╠═93002fa1-995c-4306-a6a6-264be2d939d1
-# ╠═54916f43-c05a-4ff3-bd62-ba715f0b6937
 # ╠═fc43feee-9d9a-4af6-a76a-7dfbb927c0ae
-# ╠═367a0c68-081f-4a57-9a26-9d4094b8f1df
-# ╠═e4ed2262-1d71-47ec-8256-6d5c2f8ad376
-# ╠═baaa5740-d6e6-405a-8e14-fa5b0ef7050b
 # ╟─c44b2487-bcd2-43f2-af89-2e3b0e1a54e8
 # ╟─5aecb6b9-a813-4cf8-8a7f-2da4a19a052e
 # ╟─d90057db-c68d-4b70-9247-1098bf129783
 # ╠═140c1343-2a6e-4d4f-a3db-0d608d7e885c
-# ╠═9a5dfd0a-a27a-444e-9963-621a89170652
-# ╠═3631e518-6258-4cb7-ad61-8d7975f2da67
-# ╠═89f21b6f-ecbc-475d-bf9e-408fcbc7f45a
-# ╠═d1fb1e4d-e0b5-48ce-82bd-69b3d91f2b2a
-# ╠═7b841262-0031-4a44-b17c-f5059b0f59a0
-# ╠═1b1d2d32-3411-4ecb-8558-a3bf5af10bd8
-# ╠═4cf12eb6-2739-497c-9171-5f2f557c60d0
-# ╠═72ddceea-6a7d-4b05-99dd-3313ce0eb840
 # ╟─a2aeaa04-3097-4dd0-8bab-5c98b74514b3
-# ╠═283c0ee5-0321-4f5d-9792-d593f49cafc1
-# ╠═ed06f353-fdcf-47af-94be-52c2e11957f4
-# ╠═15b31020-253d-46e6-a985-f2043c71e1fc
+# ╟─283c0ee5-0321-4f5d-9792-d593f49cafc1
 # ╟─1befeeba-40bb-4310-8441-6609fc82dc21
-# ╠═ac0e22da-0617-434c-b6ed-88c5f688f1c1
 # ╠═c0dcbc56-be6e-47ba-b3e8-9f12ec469e4b
 # ╠═e79e2a10-78d0-4571-b601-daf9d164b0c9
 # ╠═a861ded4-dbcf-4f06-a1b4-17d8f8ddf214
