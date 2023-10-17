cb30b6ea330bcc96d97732a151638611849db386
Dale-Black <djblack@uci.edu>
2023-10-17 11:08:33 -0700

    update tests on os

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,8 @@
 using PerfusionImaging
 using Test
 
-@testset "PerfusionImaging.jl" begin
-    # Write your tests here.
+@testset "registration" begin
+    fixed_image, moving_image = rand([0, 1], 10, 10, 10), rand([0, 1], 10, 10, 10)
+    registered_image = register(fixed_image, moving_image; num_iterations = 2)
+    @test size(fixed_image) == size(registered_image)
 end
